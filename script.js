865682744759d972df21acb61a2e1bcacb73b733
ryangoslinglov3r <winhardchristian@gmail.com>
2025-03-30 14:25:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function clearInput() {
 }
 
 async function getWeather(city) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
         const response = await fetch(url);
